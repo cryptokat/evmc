5a4172c7728e1a280a8f7fc0237a8221a576b9c4
Paweł Bylica <chfast@gmail.com>
2018-04-10 17:11:43 +0200

    WIP search for symbols in dlls

--- test/vmtester/vmtester.cpp
+++ test/vmtester/vmtester.cpp
@@ -2,6 +2,29 @@
 // Copyright 2018 Pawel Bylica.
 // Licensed under the MIT License. See the LICENSE file.
 
+#include <boost/dll/import.hpp> // for import_alias
+#include <boost/make_shared.hpp>
+#include <boost/function.hpp>
+#include <iostream>
+
+namespace dll = boost::dll;
+
+std::size_t
+search_for_symbols(const std::vector<boost::filesystem::path> &plugins) {
+  std::size_t plugins_found = 0;
+
+  for (std::size_t i = 0; i < plugins.size(); ++i) {
+    std::cout << "Loading plugin: " << plugins[i] << '\n';
+    dll::shared_library lib(plugins[i], dll::load_mode::append_decorations);
+    if (!lib.has("create_plugin")) {
+      // no such symbol
+      continue;
+    }
+  }
+
+  return plugins_found;
+}
+
 int main()
 {
     return 0;
